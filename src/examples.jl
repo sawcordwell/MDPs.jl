839c08bdf5fc022ef3a457a0265cbd59423c5120
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2014-06-21 14:59:34 +1200

    [examples] Allow randmdp to have 'mask' parameter
    The mask parameter is specified as the third argument to the randmdp
    function. It is a Boolean array where each 'false' value means that
    the corresponding element in the transition matrix will have a zero
    probability. Elements where there is a 'true' value will be assigned a
    random probability.

--- src/examples.jl
+++ src/examples.jl
@@ -1,14 +1,23 @@
 
 function randmdp(s::Int, a::Int)
     P = rand(s, s, a)
-    for k in range(1, a)
+    for k = 1:a
         P[:, :, k] = P[:, :, k] ./ sum(P[:, :, k], 2)
     end
     R = 1 .- 2rand(s, a)
     (P, R)
 end
 
-#function randmdp(s::Real, a::Real, mask::Array{Bool,2})
-#    #
-#end
+function randmdp(s::Real, a::Real, mask::Array{Bool,2})
+    P = rand(s, s, a)
+    for k = 1:a
+        P[:, :, k][!mask] = 0
+        P[:, :, k] = P[:, :, k] ./ sum(P[:, :, k], 2)
+    end
+    R = 1 .- 2rand(s, a)
+    (P, R)
+end
 
+function bellmanmachine(k::Int, M::Int, p::Array{Float64,2}, q::Array{Float64,2})
+    #
+end
