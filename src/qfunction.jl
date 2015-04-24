a8b8128316fd41f951c2276f3f3ba9a42d71eb8a
Steven Cordwell <steven.cordwell@uqconnect.edu.au>
2015-04-25 08:58:31 +1200

    Move abstract Q-function and transition types to their implementation files

--- src/qfunction.jl
+++ src/qfunction.jl
@@ -1,3 +1,28 @@
+# -------------
+# Abstract type
+# -------------
+
+@doc """
+# Interface
+
+where Q is a subtype of AbstractQFunction
+
+* policy(Q) -> a vector of the (currently) optimal policy for all states
+* policy(Q, s) -> the (currently) optimal action to take at state s
+* value(Q) -> a vector of the currently optimal values for all states
+* value(Q, s) -> the value expected from state s given (currently) optimal actions are taken
+* setvalue!(Q, v, s, a) -> set the value for state action
+* num_states(Q) -> the number of states
+
+## Optional
+
+* getvalue(Q, s, a) -> the value expected from state s given action a is taken
+* num_actions(Q) -> the number of actions
+
+""" ->
+abstract AbstractQFunction
+
+
 
 # ----------
 # Array type
@@ -40,6 +65,7 @@ getvalue(Q::ArrayQFunction, state, action) = getindex(Q.array, state, action)
 
 setvalue!(Q::ArrayQFunction, v, state, action) =
     (setindex!(Q.array, v, state, action); Q)
+
 
 
 # -----------
--- src/transition.jl
+++ src/transition.jl
@@ -1,11 +1,47 @@
 import Base: convert, ndims, size
 
-# Base array functions
+
+
+# --------------
+# Abstract types
+# --------------
+
+@doc """
+# Interface
+
+* probability(P, s, t, a) -> the probability of transitioning from state s to state t given action a
+
+""" ->
+abstract AbstractTransitionProbability
+
+
+@doc """
+# Interface
+
+* probability(P, a) -> probability transition matrix for action a
+
+## Optional
+
+* getindex(P, s, t, a) -> the probability of transitioning from state s to state t given action a
+
+""" ->
+abstract AbstractTransitionProbabilityArray <: AbstractTransitionProbability
+
+
+
+# --------------
+# Base functions
+# --------------
 
 ndims(P::AbstractTransitionProbabilityArray) = ndims(P.array)
 size(P::AbstractTransitionProbabilityArray) = size(P.array)
 size(P::AbstractTransitionProbabilityArray, dims...) = size(P.array, dims...)
 
+
+
+# ----------
+# Array type
+# ----------
 
 immutable TransitionProbabilityArray{T<:Real} <: AbstractTransitionProbabilityArray
     array::Array{T,3}
@@ -26,6 +62,7 @@ num_states(P::TransitionProbabilityArray) = size(P, 1)
 
 getindex(P::TransitionProbabilityArray, dims...) = getindex(P.array, dims...)
 
+
 @doc """
 # Transition probability
 
@@ -38,6 +75,11 @@ probability(P::TransitionProbabilityArray, s, t, a) = getindex(P, s, t, a)
 probability(P::TransitionProbabilityArray, a) = P[:, :, a]
 
 
+
+# -----------------
+# Sparse array type
+# -----------------
+
 immutable SparseTransitionProbabilityArray{Tv<:Real,Ti} <: AbstractTransitionProbabilityArray
     array::Vector{SparseMatrixCSC{Tv,Ti}}
 end
@@ -47,8 +89,6 @@ num_actions(P::SparseTransitionProbabilityArray) = length(P.array)
 
 num_states(P::SparseTransitionProbabilityArray) = size(getindex(P.array, 1), 1)
 
-# getindex(P::SparseTransitionProbabilityArray, a, b, c) =
-#     getindex(getindex(P.array, c), a, b)
 
 probability(P::SparseTransitionProbabilityArray, s, t, a) =
     getindex(getindex(P.array, a), s, t)
@@ -56,6 +96,10 @@ probability(P::SparseTransitionProbabilityArray, s, t, a) =
 probability(P::SparseTransitionProbabilityArray, a) = getindex(P.array, a)
 
 
+
+# -------------
+# Function type
+# -------------
 
 immutable TransitionProbabilityFunction <: AbstractTransitionProbability
     func::Function
